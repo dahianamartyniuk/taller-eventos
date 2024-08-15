0684835092bf212cc82f0a023c2d342552b9dd1e
dahianamartyniuk <dahiamarty@gmail.com>
2024-08-15 11:41:20 -0300

    Separando eventos

--- eventos.js
+++ eventos.js
@@ -2,8 +2,18 @@
 //El contenido del documento debe de estar todo cargado
 document.addEventListener('DOMContentLoaded', () => {
     const div = document.querySelector('.container');   //Accede al div - clase
+    const button = document.querySelector('.button');
 
 div.addEventListener('click', () => {           //Añade el manejador de eventos para el click 
     alert('¡Hola! Soy el div');
+
+// Manejador del evento click para el botón
+button.addEventListener('click', (event) => {
+    // Detiene la propagación del evento click hacia el div
+    event.stopPropagation();
+    alert('¡Haz clic en el botón!');
+
+
+});
 });
 });
